77760396066dbb918127ab20958f052f07b62257
Jason Smith <jasonrichardsmith@gmail.com>
2017-04-13 11:45:26 +0200

    added simple visual tests to insure getstacks is working

--- platform/docker/stacks_test.go
+++ platform/docker/stacks_test.go
@@ -1,10 +1,28 @@
 package docker
 
 import (
-	"fmt"
+	"os"
 	"testing"
+
+	"github.com/davecgh/go-spew/spew"
+	"github.com/go-kit/kit/log"
 )
 
 func TestGetStacks(t *testing.T) {
-	fmt.Println("hello")
+	var logger log.Logger
+	{
+		logger = log.NewLogfmtLogger(os.Stderr)
+		logger = log.NewContext(logger).With("ts", log.DefaultTimestampUTC)
+		logger = log.NewContext(logger).With("caller", log.DefaultCaller)
+	}
+	swarm, err := NewSwarm(logger)
+	if err != nil {
+		t.Error(err)
+	}
+	s, err := swarm.GetStacks()
+	if err != nil {
+		t.Error(err)
+	}
+	spew.Dump(s)
+
 }

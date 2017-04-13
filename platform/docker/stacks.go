0e0251718933363db1b10fcc58dbff090bca9ce5
Jason Smith <jasonrichardsmith@gmail.com>
2017-04-13 13:42:44 +0200

    removed convert dep

--- platform/docker/stacks.go
+++ platform/docker/stacks.go
@@ -5,7 +5,6 @@ import (
 	"time"
 
 	"github.com/docker/docker/api/types"
-	"github.com/docker/docker/cli/compose/convert"
 	"github.com/pkg/errors"
 )
 
@@ -15,6 +14,11 @@ type stack struct {
 	// Services is the number of the services
 	Services int
 }
+
+const (
+	// LabelNamespace is the label used to track stack resources
+	LabelNamespace = "com.docker.stack.namespace"
+)
 
 func (c *Swarm) GetStacks() ([]*stack, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
@@ -30,10 +34,10 @@ func (c *Swarm) GetStacks() ([]*stack, error) {
 	m := make(map[string]*stack, 0)
 	for _, service := range services {
 		labels := service.Spec.Labels
-		name, ok := labels[convert.LabelNamespace]
+		name, ok := labels[LabelNamespace]
 		if !ok {
 			return nil, errors.Errorf("cannot get label %s for service %s",
-				convert.LabelNamespace, service.ID)
+				LabelNamespace, service.ID)
 		}
 		ztack, ok := m[name]
 		if !ok {

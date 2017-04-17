9532d2ab6b1a6f4b2ae517a469ffc3898f00b920
Luke Marsden <me@lukemarsden.net>
2017-04-17 11:31:13 -0500

    Moar debug.

--- platform/docker/services.go
+++ platform/docker/services.go
@@ -64,8 +64,8 @@ func (c *Swarm) AllServices(namespace string, ignore flux.ServiceIDSet) ([]platf
 
 		/// LOG
 		fmt.Printf(
-			"[AllServices] %s: OLD: %v, NEW: %v\n",
-			v.Spec.Name, oldPcs, pcs,
+			"[AllServices] %s: OLD: %v, NEW: %v; ts=%v\n",
+			v.Spec.Name, oldPcs, pcs, ts,
 		)
 
 		ps.Containers.Containers = pcs
